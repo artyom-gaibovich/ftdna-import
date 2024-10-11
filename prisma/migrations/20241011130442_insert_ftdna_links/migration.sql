56495650b91fd514f82724ffa59869aa50734602
Artem <artyomguybov2002@gmail.com>
2024-10-11 16:15:55 +0300

    added migration for insert data into ftdna links

---
+++ prisma/migrations/20241011130442_insert_ftdna_links/migration.sql
@@ -0,0 +1,124 @@
+INSERT INTO public.ftdna_links (url,target) VALUES
+	 ('https://www.familytreedna.com/public/abaza-ethnic-group?iframe=yresults','abaza-ethnic-group'),
+	 ('https://www.familytreedna.com/public/jewish-polesie?iframe=yresults','jewish-polesie'),
+	 ('https://www.familytreedna.com/public/balticsea?iframe=yresults','balticsea'),
+	 ('https://www.familytreedna.com/public/belarusian-nobility-dna?iframe=yresults','belarusian-nobility-dna'),
+	 ('https://www.familytreedna.com/public/ashina-royal-dynasty?iframe=yresults','ashina-royal-dynasty'),
+	 ('https://www.familytreedna.com/public/bashkortostan?iframe=yresults','bashkortostan'),
+	 ('https://www.familytreedna.com/public/cossack-dna?iframe=yresults','cossack-dna'),
+	 ('https://www.familytreedna.com/public/ukraine-kyiv-kyivska?iframe=yresults','ukraine-kyiv-kyivska'),
+	 ('https://www.familytreedna.com/public/karjaladna?iframe=yresults','karjaladna'),
+	 ('https://www.familytreedna.com/public/k-balkar-dna?iframe=yresults','k-balkar-dna');
+INSERT INTO public.ftdna_links (url,target) VALUES
+	 ('https://www.familytreedna.com/public/nntamim?iframe=yresults','nntamim'),
+	 ('https://www.familytreedna.com/public/central-asian-big-ys?iframe=yresults','central-asian-big-ys'),
+	 ('https://www.familytreedna.com/public/kazakhstan?iframe=yresults','kazakhstan'),
+	 ('https://www.familytreedna.com/public/g1?iframe=yresults','g1'),
+	 ('https://www.familytreedna.com/public/adyghe?iframe=yresults','adyghe'),
+	 ('https://www.familytreedna.com/public/abkhazian-dna-project?iframe=yresults','abkhazian-dna-project'),
+	 ('https://www.familytreedna.com/public/armeniadnaproject?iframe=yresults','armeniadnaproject'),
+	 ('https://www.familytreedna.com/public/azov-greeks?iframe=yresults','azov-greeks'),
+	 ('https://www.familytreedna.com/public/scandinavianydna?iframe=yresults','scandinavianydna'),
+	 ('https://www.familytreedna.com/public/khazars?iframe=yresults','khazars');
+INSERT INTO public.ftdna_links (url,target) VALUES
+	 ('https://www.familytreedna.com/public/megalithic-caucasus?iframe=yresults','megalithic-caucasus'),
+	 ('https://www.familytreedna.com/public/ponticandanatoliangreeksdna?iframe=yresults','ponticandanatoliangreeksdna'),
+	 ('https://www.familytreedna.com/public/belarus-lida-region?iframe=yresults','belarus-lida-region'),
+	 ('https://www.familytreedna.com/public/balto-slavic-dna?iframe=yresults','balto-slavic-dna'),
+	 ('https://www.familytreedna.com/public/kirgiz?iframe=yresults','kirgiz'),
+	 ('https://www.familytreedna.com/public/hungarian-magyar-y-dna-project?iframe=yresults','hungarian-magyar-y-dna-project'),
+	 ('https://www.familytreedna.com/public/anatol-balkan-caucas?iframe=yresults','anatol-balkan-caucas'),
+	 ('https://www.familytreedna.com/public/carpatho-rusyn?iframe=yresults','carpatho-rusyn'),
+	 ('https://www.familytreedna.com/public/caucasian-albania?iframe=yresults','caucasian-albania'),
+	 ('https://www.familytreedna.com/public/balkangenetics?iframe=yresults','balkangenetics');
+INSERT INTO public.ftdna_links (url,target) VALUES
+	 ('https://www.familytreedna.com/public/new-netherlanders?iframe=yresults','new-netherlanders'),
+	 ('https://www.familytreedna.com/public/caucasus?iframe=yresults','caucasus'),
+	 ('https://www.familytreedna.com/public/chechen-noahcho?iframe=yresults','chechen-noahcho'),
+	 ('https://www.familytreedna.com/public/czech?iframe=yresults','czech'),
+	 ('https://www.familytreedna.com/public/chechersk-dna?iframe=yresults','chechersk-dna'),
+	 ('https://www.familytreedna.com/public/dagestan?iframe=yresults','dagestan'),
+	 ('https://www.familytreedna.com/public/estonia?iframe=yresults','estonia'),
+	 ('https://www.familytreedna.com/public/cossack-dna?iframe=yresults','cossack-dna'),
+	 ('https://www.familytreedna.com/public/croatian-dna?iframe=yresults','croatian-dna'),
+	 ('https://www.familytreedna.com/public/dankov-dna-project?iframe=yresults','dankov-dna-project');
+INSERT INTO public.ftdna_links (url,target) VALUES
+	 ('https://www.familytreedna.com/public/georgia?iframe=yresults','georgia'),
+	 ('https://www.familytreedna.com/public/Circassian?iframe=yresults','Circassian'),
+	 ('https://www.familytreedna.com/public/finland?iframe=yresults','finland'),
+	 ('https://www.familytreedna.com/public/crimean-tatar-dna?iframe=yresults','crimean-tatar-dna'),
+	 ('https://www.familytreedna.com/public/greece?iframe=yresults','greece'),
+	 ('https://www.familytreedna.com/public/ingush?iframe=yresults','ingush'),
+	 ('https://www.familytreedna.com/public/jewish-ukraine-west?iframe=yresults','jewish-ukraine-west'),
+	 ('https://www.familytreedna.com/public/karachev-dna-project?iframe=yresults','karachev-dna-project'),
+	 ('https://www.familytreedna.com/public/nakhdna?iframe=yresults','nakhdna'),
+	 ('https://www.familytreedna.com/public/kairafinlanddna?iframe=yresults','kairafinlanddna');
+INSERT INTO public.ftdna_links (url,target) VALUES
+	 ('https://www.familytreedna.com/public/kazakhstan?iframe=yresults','kazakhstan'),
+	 ('https://www.familytreedna.com/public/kazakhstan-dna?iframe=yresults','kazakhstan-dna'),
+	 ('https://www.familytreedna.com/public/lipetsk?iframe=yresults','lipetsk'),
+	 ('https://www.familytreedna.com/public/latvia?iframe=yresults','latvia'),
+	 ('https://www.familytreedna.com/public/romania?iframe=yresults','romania'),
+	 ('https://www.familytreedna.com/public/russian-impire-aspx?iframe=yresults','russian-impire-aspx'),
+	 ('https://www.familytreedna.com/public/rsl?iframe=yresults','rsl'),
+	 ('https://www.familytreedna.com/public/russian-ethnic-project?iframe=yresults','russian-ethnic-project'),
+	 ('https://www.familytreedna.com/public/russiadna?iframe=yresults','russiadna'),
+	 ('https://www.familytreedna.com/public/russian-nobility-dna?iframe=yresults','russian-nobility-dna');
+INSERT INTO public.ftdna_links (url,target) VALUES
+	 ('https://www.familytreedna.com/public/turkey?iframe=yresults','turkey'),
+	 ('https://www.familytreedna.com/public/russian-dna?iframe=yresults','russian-dna'),
+	 ('https://www.familytreedna.com/public/siberia-transbaikalia?iframe=yresults','siberia-transbaikalia'),
+	 ('https://www.familytreedna.com/public/ryazan-dna?iframe=yresults','ryazan-dna'),
+	 ('https://www.familytreedna.com/public/russian-princes?iframe=yresults','russian-princes'),
+	 ('https://www.familytreedna.com/public/ukraine?iframe=yresults','ukraine'),
+	 ('https://www.familytreedna.com/public/tatarstan?iframe=yresults','tatarstan'),
+	 ('https://www.familytreedna.com/public/saami?iframe=yresults','saami'),
+	 ('https://www.familytreedna.com/public/volga-germans?iframe=yresults','volga-germans'),
+	 ('https://www.familytreedna.com/public/c3?iframe=yresults','c3');
+INSERT INTO public.ftdna_links (url,target) VALUES
+	 ('https://www.familytreedna.com/public/ukrainian-dna?iframe=yresults','ukrainian-dna'),
+	 ('https://www.familytreedna.com/public/komi?iframe=yresults','komi'),
+	 ('https://www.familytreedna.com/public/i-1suomi?iframe=yresults','i-1suomi'),
+	 ('https://www.familytreedna.com/public/2121?iframe=yresults','2121'),
+	 ('https://www.familytreedna.com/public/g-ydna?iframe=yresults','g-ydna'),
+	 ('https://www.familytreedna.com/public/y-dna-q?iframe=yresults','y-dna-q'),
+	 ('https://www.familytreedna.com/public/serbian-dna-project?iframe=yresults','serbian-dna-project'),
+	 ('https://www.familytreedna.com/public/karachev-dna-project?iframe=yresults','karachev-dna-project'),
+	 ('https://www.familytreedna.com/public/balkangenetics?iframe=yresults','balkangenetics'),
+	 ('https://www.familytreedna.com/public/kursk-dna-project?iframe=yresults','kursk-dna-project');
+INSERT INTO public.ftdna_links (url,target) VALUES
+	 ('https://www.familytreedna.com/public/cossacks?iframe=yresults','cossacks'),
+	 ('https://www.familytreedna.com/public/udmurt-beserman?iframe=yresults','udmurt-beserman'),
+	 ('https://www.familytreedna.com/public/korea-dna?iframe=yresults','korea-dna'),
+	 ('https://www.familytreedna.com/public/japan?iframe=yresults','japan'),
+	 ('https://www.familytreedna.com/public/y-dna-i1?iframe=yresults','y-dna-i1'),
+	 ('https://www.familytreedna.com/public/white-sea-karelian-families?iframe=yresults','white-sea-karelian-families'),
+	 ('https://www.familytreedna.com/public/i1-z138y-dna?iframe=yresults','i1-z138y-dna'),
+	 ('https://www.familytreedna.com/public/kostroma?iframe=yresults','kostroma'),
+	 ('https://www.familytreedna.com/public/iberian-surnamesof-ashkenaz?iframe=yresults','iberian-surnamesof-ashkenaz'),
+	 ('https://www.familytreedna.com/public/germany','germany');
+INSERT INTO public.ftdna_links (url,target) VALUES
+	 ('https://www.familytreedna.com/public/pontic-greek?iframe=yresults','pontic-greek'),
+	 ('https://www.familytreedna.com/public/russia?iframe=yresults','russia'),
+	 ('https://www.familytreedna.com/public/bulgariandna?iframe=yresults','bulgariandna'),
+	 ('https://www.familytreedna.com/public/vyatskya-gubarnya?iframe=yresults','vyatskya-gubarnya'),
+	 ('https://www.familytreedna.com/public/kazakhstan-dna?iframe=yresults','kazakhstan-dna'),
+	 ('https://www.familytreedna.com/public/bosniak-dn-aproject?iframe=yresults','bosniak-dn-aproject'),
+	 ('https://www.familytreedna.com/public/i-2a-l161?iframe=yresults','i-2a-l161'),
+	 ('https://www.familytreedna.com/public/m223-y-clan?iframe=yresults','m223-y-clan'),
+	 ('https://www.familytreedna.com/public/i-1d?iframe=yresults','i-1d'),
+	 ('https://www.familytreedna.com/public/e-1b-1arabia?iframe=yresults','e-1b-1arabia');
+INSERT INTO public.ftdna_links (url,target) VALUES
+	 ('https://www.familytreedna.com/public/bashqort-tribes-and-clans?iframe=yresults','bashqort-tribes-and-clans'),
+	 ('https://www.familytreedna.com/public/e-v13?iframe=yresults','e-v13'),
+	 ('https://www.familytreedna.com/public/nashe-opolie?iframe=yresults','nashe-opolie'),
+	 ('https://www.familytreedna.com/public/kavkaz?iframe=yresults','kavkaz'),
+	 ('https://www.familytreedna.com/public/z140?iframe=yresults','z140'),
+	 ('https://www.familytreedna.com/public/i-2a-hap-group?iframe=yresults','i-2a-hap-group'),
+	 ('https://www.familytreedna.com/public/chaplogroup?iframe=yresults','chaplogroup'),
+	 ('https://www.familytreedna.com/public/g-z6552?iframe=yresults','g-z6552'),
+	 ('https://www.familytreedna.com/public/i1-ee?iframe=yresults','i1-ee'),
+	 ('https://www.familytreedna.com/public/belarus?iframe=yresults','belarus');
+INSERT INTO public.ftdna_links (url,target) VALUES
+	 ('https://www.familytreedna.com/public/arcadia?iframe=yresults','arcadia'),
+	 ('https://www.familytreedna.com/public/finno-ugric-dna?iframe=yresults','finno-ugric-dna');
